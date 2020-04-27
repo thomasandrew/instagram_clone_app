dcd92b6d44b7a3180a260cd5454dfaf110c16403
Thomas <53497553+thomasandrew@users.noreply.github.com>
2020-04-26 21:15:31 -0300

    Create Stories.js

---
+++ screens/Stories.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react'
+import {
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+    Image,
+    TouchableOpacity,
+} from 'react-native'
+import LinearGradient from 'react-native-linear-gradient';
+
+
+
+export default class App extends Component {
+    render() {
+        return (
+            <View>
+                <TouchableOpacity>
+                    <View style={styles.insideScrollView}>
+                        <LinearGradient
+                            colors={['#CA1D7E', '#E35157', '#F2703F']}
+                            start={{ x: 0.0, y: 1.0 }} end={{ x: 1.0, y: 1.0 }}
+                            style={{
+                                height: 54,
+                                width: 54,
+                                alignItems: 'center',
+                                justifyContent: 'center',
+                                borderRadius: 100,
+                                marginLeft: 15,
+                                marginTop: 3,
+                            }}>
+                            <Image source={{ uri: this.props.users }}
+                                style={{
+                                    width: 50,
+                                    height: 50,
+                                    borderRadius: 75 / 2,
+                                    alignSelf: 'center',
+                                    borderColor: '#fff',
+                                    borderWidth: 3
+                                }} />
+                        </LinearGradient>
+                    </View>
+                    <View style={styles.names}>
+                        <Text>
+                            {this.props.name}
+                        </Text>
+                    </View>
+                </TouchableOpacity>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    names: {
+        marginLeft: 27,
+        fontFamily: 'Verdana',
+    },
+})
